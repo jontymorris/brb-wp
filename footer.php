4fd488b090cde3e9f7bfb1376551f2dc66f885d2
Jonty <jonty.morris@yahoo.co.nz>
2020-03-28 22:16:40 +1300

    Spaced the social icons

--- footer.php
+++ footer.php
@@ -1,21 +1,21 @@
 <div class="container-fluid" id="footer">
 	<div class="container">
 		<div class="row">
-			<div class="col-sm-3">
+			<div class="col-sm-3 mb-2">
 				Keep up with the Backridge Butcher on <strong>Facebook</strong>, <strong>Youtube</strong> and <strong>Instagram</strong>.
 			</div>
 			<div class="col-md-3"></div>
-			<div class="col-sm-2">
+			<div class="col-sm-2 mb-3">
 				<a href="https://www.facebook.com/backridge.butcher" target="_blank">
 					<img src="<?php echo get_template_directory_uri().'/images/facebook.png'; ?>" class="social-image">
 				</a>
 			</div>
-			<div class="col-sm-2">
+			<div class="col-sm-2 mb-3">
 				<a href="https://www.youtube.com/c/BackridgeButcher" target="_blank">
 					<img src="<?php echo get_template_directory_uri().'/images/youtube.png'; ?>" class="social-image">
 				</a>
 			</div>
-			<div class="col-sm-2">
+			<div class="col-sm-2 mb-3">
 				<a href="https://www.instagram.com/backridgebutcher/" target="_blank" width="75px">
 					<img src="<?php echo get_template_directory_uri().'/images/instagram.png'; ?>" class="social-image">
 				</a>
