73d960a6d71c8cee392d6b787e0d52a050285e5c
Jonty <jonty.morris@yahoo.co.nz>
2020-03-28 17:47:39 +1300

    Increased footer space

--- footer.php
+++ footer.php
@@ -4,17 +4,18 @@
 			<div class="col-sm-3">
 				Keep up with the Backridge Butcher on <strong>Facebook</strong>, <strong>Youtube</strong> and <strong>Instagram</strong>.
 			</div>
-			<div class="col-sm-3">
+			<div class="col-md-3"></div>
+			<div class="col-sm-2">
 				<a href="https://www.facebook.com/backridge.butcher" target="_blank">
 					<img src="<?php echo get_template_directory_uri().'/images/facebook.png'; ?>" class="social-image">
 				</a>
 			</div>
-			<div class="col-sm-3">
+			<div class="col-sm-2">
 				<a href="https://www.youtube.com/c/BackridgeButcher" target="_blank">
 					<img src="<?php echo get_template_directory_uri().'/images/youtube.png'; ?>" class="social-image">
 				</a>
 			</div>
-			<div class="col-sm-3">
+			<div class="col-sm-2">
 				<a href="https://www.instagram.com/backridgebutcher/" target="_blank" width="75px">
 					<img src="<?php echo get_template_directory_uri().'/images/instagram.png'; ?>" class="social-image">
 				</a>
